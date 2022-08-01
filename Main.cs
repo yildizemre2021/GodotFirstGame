609eec409c3d5dcfc535c069ddcd86f830ea5c2e
yıldızemre2021 <yildiz.emre.2021@gmail.com>
2022-08-01 22:03:40 +0300

    Create Main Node

--- Main.cs
+++ Main.cs
@@ -1,24 +1,22 @@
 using Godot;
 using System;
 
-public class Mob : RigidBody2D
+public class Main : Node
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
+#pragma warning disable 649
+    [Export]
+    public PackedScene MobScene;
+#pragma warning restore 649
+
+    public int Score;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        var animSprite = GetNode<AnimatedSprite>("AnimatedSprite");
-        animSprite.Playing = true;
-        string[] mobTypes = animSprite.Frames.GetAnimationNames();
-        animSprite.Animation = mobTypes[GD.Randi() % mobTypes.Length];
-    }
-
-    public void OnVisibilityNotifier2DScreenExited()
-    {
-        QueueFree();
+        GD.Randomize();   
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
