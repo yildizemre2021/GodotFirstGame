b51011fbeb894e2db68916b826db4b1a6d73775c
yıldızemre2021 <yildiz.emre.2021@gmail.com>
2022-08-01 21:16:16 +0300

    Add Mob codes

---
+++ Mob.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class Mob : RigidBody2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var animSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        animSprite.Playing = true;
+        string[] mobTypes = animSprite.Frames.GetAnimationNames();
+        animSprite.Animation = mobTypes[GD.Randi() % mobTypes.Length];
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
