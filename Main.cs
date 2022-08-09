db5511c55b787409bd2fc0f8b89ebe78e46003c6
yildizemre2021 <yildiz.emre.2021@gmail.com>
2022-08-09 22:06:18 +0300

    Add NewGame Method

--- Main.cs
+++ Main.cs
@@ -16,12 +16,72 @@ public class Main : Node
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Randomize();   
+        GD.Randomize();
+        NewGame();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
+    //  
+    public void GameOver()
+    {
+        GetNode<Timer>("MobTimer").Stop();
+        GetNode<Timer>("ScoreTimer").Stop();
+    }
+
+    public void NewGame()
+    {
+        Score = 0;
+
+        var player = GetNode<Player>("Player");
+        var startPosition = GetNode<Position2D>("StartPosition");
+        player.Start(startPosition.Position);
+
+        GetNode<Timer>("StartTimer").Start();
+    }
+
+    public void OnScoreTimerTimeout()
+    {
+        Score++;
+    }
+
+    public void OnStartTimerTimeout()
+    {
+        GetNode<Timer>("MobTimer").Start();
+        GetNode<Timer>("ScoreTimer").Start();
+    }
+
+    public void OnMobTimerTimeout()
+    {
+        // Note: Normally it is best to use explicit types rather than the `var`
+        // keyword. However, var is acceptable to use here because the types are
+        // obviously Mob and PathFollow2D, since they appear later on the line.
+
+        // Create a new instance of the Mob scene.
+        var mob = (Mob)MobScene.Instance();
+
+        // Choose a random location on Path2D.
+        var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+        mobSpawnLocation.Offset = GD.Randi();
+
+        // Set the mob's direction perpendicular to the path direction.
+        float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+        // Set the mob's position to a random location.
+        mob.Position = mobSpawnLocation.Position;
+
+        // Add some randomness to the direction.
+        direction += (float)GD.RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
+        mob.Rotation = direction;
+
+        // Choose the velocity.
+        var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+        mob.LinearVelocity = velocity.Rotated(direction);
+
+        // Spawn the mob by adding it to the Main scene.
+        AddChild(mob);
+    }
 }
--- Player.cs
+++ Player.cs
@@ -3,60 +3,103 @@ using System;
 
 public class Player : Area2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-	[Export]
-	public int Speed = 400;
+    [Signal]
+    public delegate void Hit();
 
-	public Vector2 ScreenSize;
-	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ScreenSize = GetViewportRect().Size;	
-	}
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    [Export]
+    public int Speed = 400;
 
-  // Called every frame. 'delta' is the elapsed time since the previous frame.
-  public override void _Process(float delta)
-	{
-		var velocity = Vector2.Zero;
-	
-		if(Input.IsActionPressed("move_right"))
-		{
-			velocity.x += 1;
-		}
+    public Vector2 ScreenSize;
 
-		if(Input.IsActionPressed("move_left"))
-		{
-			velocity.x -= 1;
-		}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        ScreenSize = GetViewportRect().Size;
+    }
 
-		if(Input.IsActionPressed("move_up"))
-		{
-			velocity.y -= 1;
-		}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        var velocity = Vector2.Zero;
 
-		if(Input.IsActionPressed("move_down"))
-		{
-			velocity.y += 1;
-		}
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.x += 1;
+        }
 
-		var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.x -= 1;
+        }
 
-		if(velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-			animatedSprite.Play();
-		}
-		else
-		{
-			animatedSprite.Stop();
-		}
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.y -= 1;
+        }
 
-		Position += velocity * delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
-			y: Mathf.Clamp(Position.y, 0, ScreenSize.y));
-	}
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.y += 1;
+        }
+
+        var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * Speed;
+            animatedSprite.Play();
+        }
+        else
+        {
+            animatedSprite.Stop();
+        }
+
+        Position += velocity * delta;
+        Position = new Vector2(
+            x: Mathf.Clamp(Position.x, 0, ScreenSize.x),
+            y: Mathf.Clamp(Position.y, 0, ScreenSize.y));
+
+        if (velocity.x != 0)
+        {
+            animatedSprite.Animation = "walk";
+            animatedSprite.FlipV = false;
+            animatedSprite.FlipH = velocity.x < 0;
+        }
+        else if (velocity.y != 0)
+        {
+            animatedSprite.Animation = "up";
+            animatedSprite.FlipV = velocity.y > 0;
+        }
+
+        if (velocity.x < 0)
+        {
+            animatedSprite.FlipH = true;
+        }
+        else
+        {
+            animatedSprite.FlipH = false;
+        }
+    }
+
+    public void OnPlayerBodyEntered(PhysicsBody2D body)
+    {
+        Hide(); // Player disappears after being hit.
+        EmitSignal(nameof(Hit));
+        // Must be deferred as we can't change physics properties on a physics callback.
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
+    }
+
+    public void Start(Vector2 pos)
+    {
+        Position = pos;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+    }
+
+
+
+
 }
